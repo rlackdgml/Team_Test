5b68eb0afb1ab9fcfdd872bca94b8f5fcadea23b
rlackdgml <dhdlqkqh2003@naver.com>
2020-08-10 15:19:21 +0900

    asd

--- src/main/java/com/biz/controller/BookController.java
+++ src/main/java/com/biz/controller/BookController.java
@@ -1,5 +1,12 @@
 package com.biz.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class BookController {
+	
+	public String List() {
+		return "book/list" ;
+	}
 
 }
--- src/main/java/com/biz/service/BookService.java
+++ src/main/java/com/biz/service/BookService.java
@@ -1,5 +1,12 @@
 package com.biz.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookService {
+	
+	public void list() {
+		System.out.println("도서 리스트 가져오기");
+	}
 
 }
